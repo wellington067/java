a4da3afb71eb12615af2b6ced078ffbabf9debb6
José Wellington <109475692+wellington067@users.noreply.github.com>
2022-08-23 21:36:31 -0300

    Create criando_pacotes.java

---
+++ criando_pacotes.java
@@ -0,0 +1,25 @@
+//Crie dois pacotes baseados nas atribuições de uma empresa e inclua classes neles. Lembre-se de trabalhar com os padrões Java.
+
+
+package empresa_x.funcionarios;
+
+public class funcionarios {
+  
+  }
+public class salario_por_funcionario{
+
+}
+
+public class cargos_por_funcionarios{
+  
+}
+
+
+package empresa_x.relatorio_de_gastos;
+
+public class gasto_manutençao{
+
+}
+public class gasto_equipamentos{
+ 
+}
