1bd48f9cf6cd939e97f8b002f57176b9141608b0
José Wellington <109475692+wellington067@users.noreply.github.com>
2022-08-23 21:45:44 -0300

    Update criando_pacotes.java

--- criando_pacotes.java
+++ criando_pacotes.java
@@ -5,14 +5,14 @@ package empresa_x.funcionarios;
 
 public class funcionarios {
   
-  }
+}
 public class salario_por_funcionario{
 
 }
-
 public class cargos_por_funcionarios{
   
 }
+
 
 
 package empresa_x.relatorio_de_gastos;
