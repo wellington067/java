f8348fbdbeb84220607d8f1ccd2e223f4b43a07f
José Wellington <109475692+wellington067@users.noreply.github.com>
2022-08-24 23:36:57 -0300

    Create tratando_exceçoes.java
    Crie uma situação em que ocorra uma exceção dentro de um código. Utilize try/catch para realizar a captura e tratamento dessa exceção.

---
+++ tratando_exceçoes.java
@@ -0,0 +1,16 @@
+try
+{
+  //trecho de código que pode vir a lançar uma exceção
+}
+catch(tipo_exceçao_1 e)
+{
+  //ação a ser tomada
+}
+catch(tipo_exceçao_2 e)
+{
+  //ação a ser tomada
+}
+catch(tipo_exceçao_n e)
+{
+  //ação a ser tomada
+}
